f242803d61cbc6031eb97029e67c8363cf5af9f5
Richard Doktorics <rdoktorics@hortonworks.com>
2017-08-21 14:32:45 +0200

    init

---
+++ cloud-admin/src/main/java/com/sequenceiq/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.sequenceiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
